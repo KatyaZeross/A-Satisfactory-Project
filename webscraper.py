87d7f9fa40a72b51ff013bf52a86bf8aaa2ac8e9
KatyaZeross <hello@katyakoganconsulting.com>
2021-08-16 14:07:12 -0400

    organized files

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,15 +5,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.options import Optionscon
 import time
 import urllib.request
-# import argh
 import csv
 import json
 import os
 import pandas as pd
 import numpy as np
+import re 
+from datetime import datetime
+from openpyxl import Workbook
+import csv
 
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36')]
@@ -26,77 +27,90 @@ content = driver.find_element_by_id('AppHubCards')
 
 html = driver.find_element_by_tag_name('html')
 
+# OUR GAME ID: currently, set to Satisfactory
+game_id = 526870
 
-# review_template = {
-#     'date': 'review_date',
-#     'text': 'review_text',
-#     'hours_played': 'hours_played',
-#     'recommended': 'recommended',
-#     'received_free': 'received_free'
-# }
+template = 'https://steamcommunity.com/app/{}/reviews/?browsefilter=mostrecent'
+template_with_language = 'https://steamcommunity.com/app/{}/reviews/?browsefilter=mostrecent&filterLanguage=english'
+url = template_with_language.format(game_id)
 
-temp_list = []
+driver.get(url)
 
-scrolls = 1
+# get current position of y scrollbar
+last_position = driver.execute_script("return window.pageYOffset;")
 
+reviews = []
+review_ids = set()
+running = True
 
-for scroll in range(scrolls):
-    page_num = scroll + 1
-    
-    page_name = 'page' + str(page_num)
-    page = content.find_element_by_id(page_name)
-    
-    rows = page.find_elements_by_class_name('apphub_CardRow')
-    html.send_keys(Keys.END)
-    
-    for row in rows:
+while running:
+    # get cards on the page
+    cards = driver.find_elements_by_class_name('apphub_Card')
+
+    for card in cards[-20:]:  # only the tail end are new cards
+
+        # gamer profile url
+        profile_url = card.find_element_by_xpath('.//div[@class="apphub_friend_block"]/div/a[2]').get_attribute('href')
+
+        # steam id
+        steam_id = profile_url.split('/')[-2]
         
-        cards = driver.find_elements_by_class_name('apphub_UserReviewCardContent')
-    
-        for card in cards:
-            
-            # top header
-            header = card.find_element_by_class_name('reviewInfo')
-            
-            #review content box
-            review_content = card.find_element_by_class_name('apphub_CardTextContent')
-            
-            try: 
-                received_free = review_content.find_element_by_class_name('received_compensation').text
-            except Exception as e:
-                received_free = '0'
-                pass 
+        # check to see if I've already collected this review
+        if steam_id in review_ids:
+            continue
+        else:
+            review_ids.add(steam_id)
+
+        # username
+        user_name = card.find_element_by_xpath('.//div[@class="apphub_friend_block"]/div/a[2]').text
+
+        # language of the review
+        date_posted = card.find_element_by_xpath('.//div[@class="apphub_CardTextContent"]/div').text
+        review_content = card.find_element_by_xpath('.//div[@class="apphub_CardTextContent"]').text.replace(date_posted,'').strip()    
+
+        # review length
+        review_length = len(review_content.replace(' ', ''))    
+
+        # recommendation
+        thumb_text = card.find_element_by_xpath('.//div[@class="reviewInfo"]/div[2]').text
+        thumb_text    
+
+        # amount of play hours
+        play_hours = card.find_element_by_xpath('.//div[@class="reviewInfo"]/div[3]').text
+        play_hours    
+
+        # save review
+        review = (steam_id, profile_url, review_content, thumb_text, review_length, play_hours, date_posted)
+        reviews.append(review)    
         
-            finally: 
-                review_date = review_content.find_element_by_class_name('date_posted').text
-                review_text: str = review_content.text
-                hours_played: str = header.find_element_by_class_name('hours').text
-                recommended: str = header.find_element_by_class_name('title').text
-            
-            print(review_date, review_text, hours_played, '\n')
-            
-            table_dict = {
-                'date': review_date,
-                'text': review_text,
-                'hours_played': hours_played,
-                'recommended': recommended,
-                'received_free': received_free,
-            }
+    # attempt to scroll down thrice.. then break
+    scroll_attempt = 0
 
-            temp_list.append(table_dict)
-            df = pd.DataFrame(temp_list)
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")    
+        time.sleep(2)
+        curr_position = driver.execute_script("return window.pageYOffset;")
+        
+        if curr_position == last_position:
+            scroll_attempt += 1
+            time.sleep(2)
             
-            df.to_csv(r"C:\Users\Katya\Documents\GitHub\SF-Project\scraped_data.csv")
-            # # review_df = pd.DataFrame({
-            #     'date': review_date,
-            #     'text': review_text,
-            #     'hours_played': hours_played,
-            #     'recommended': recommended,
-            #     'received_free': received_free,
-            # # }, index=[0])
-
-
-            
-# pd.DataFrame(review_df, index=[]).to_csv(page_name + '.csv')
+            if curr_position >= 3:
+                running = False
+                break
+        else:
+            last_position = curr_position
+            break  # continue scraping the results
+        
+    #TO DO: condition when "See More Content" button appears
+driver.find_element_by_xpath('.//*[@id="GetMoreContentBtn"]/a').click()
+# shutdown the web driver
 driver.close()
+
+# save the file to a CSV file
+today = datetime.today().strftime('%Y%m%d')   
+with open(f'Steam_Reviews_{game_id}_{today}.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['SteamId', 'ProfileURL', 'ReviewText', 'Review', 'ReviewLength(Chars)', 'PlayHours', 'DatePosted'])
+    writer.writerows(reviews)
 
